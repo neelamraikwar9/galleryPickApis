abbf8e6f2d48396f55629f9274cee4e8e1c3ede9
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-02-15 17:33:50 +0530

    feat: Adde google auth API

--- index.js
+++ index.js
@@ -3,34 +3,35 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const galeryUser = require("./models/User.model"); 
-const dotenv = require("dotenv"); 
+const galeryUser = require("./models/User.model");
+const dotenv = require("dotenv");
 const { initializeDB } = require("./db.connect");
+const axios = require("axios");
 
 app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
-dotenv.config(); 
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-app.post("/signup", async(req, res) => {
-    try{
-        const { name, email, password } = req.body; 
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-        const hashed = await bcrypt.hash(password, 10);
-        console.log(hashed, "hashed") 
-        const user = new galeryUser({name, email, password: hashed});
-        console.log(user, "user"); 
-        await user.save(); 
-        res.status(201).json({message: "User created."}); 
-    } catch(error){
-        console.log(error, "error"); 
-        res.status(400).json({error: "User creation failed"}); 
-    }
-}); 
+    const hashed = await bcrypt.hash(password, 10);
+    console.log(hashed, "hashed");
+    const user = new galeryUser({ name, email, password: hashed });
+    console.log(user, "user");
+    await user.save();
+    res.status(201).json({ message: "User created." });
+  } catch (error) {
+    console.log(error, "error");
+    res.status(400).json({ error: "User creation failed" });
+  }
+});
 
-//middle ware for json web token; 
+//middle ware for json web token;
 
 const verifyJWT = (req, res, next) => {
   const token = req.headers["authorization"];
@@ -49,10 +50,9 @@ const verifyJWT = (req, res, next) => {
   }
 };
 
-
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  
+
   const user = await galeryUser.findOne({ email });
 
   if (!user) {
@@ -70,14 +70,53 @@ app.post("/login", async (req, res) => {
   res.json({ token });
 });
 
-// Protected Route Example; 
+// Protected Route Example;
 app.get("/private", verifyJWT, (req, res) => {
   res.json({ message: "Welcome to the private route!", user: req.user });
 });
 
 initializeDB();
 
-const PORT = 4000; 
+//Apis for google oAuth;
+
+app.get("/auth/google", (req, res) => {
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email.`;
+
+  res.redirect(googleAuthUrl);
+});
+
+app.get("/auth/google/callback", async (req, res) => {
+  const { code } = res.query;
+  if (!code) {
+    return res.status(400).send("Authorization code not provided.");
+  }
+
+  let accessToken;
+
+  try {
+    const tokenResponse = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+      },
+      {
+        headers: {'Content-Type': "application/x-www-form-urlencoded"}, 
+      },
+    );
+
+    accessToken = tokenResponse.data.access_token; 
+    res.cookie('access_token', accessToken); 
+    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`); 
+  } catch (error) {
+    console.error(error); 
+  }
+});
+
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
 });
