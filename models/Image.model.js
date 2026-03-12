b90cf34c138e77389bb63075320c292b8103f227
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-03-12 19:01:02 +0530

    refactor: modified image schema

--- models/Image.model.js
+++ models/Image.model.js
@@ -1,71 +1,73 @@
 const { mongoose } = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
-const ImageSchema = new mongoose.Schema({
-  imageId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-
-  albumId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Album",
-    required: true,
-  },
-
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  tags: [
-    {
+const ImageSchema = new mongoose.Schema(
+  {
+    imageId: {
       type: String,
+      required: true,
+      required: true,
+      default: () => uuidv4(),
+    },
+
+    albumId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Album",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
       trim: true,
     },
-  ],
 
-  person: {
-    type: String,
-  },
-
-  isFavorite: {
-    type: Boolean,
-    default: false,
-  },
-
-  comments: [
-    {
-      userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+    tags: [
+      {
+        type: String,
+        trim: true,
       },
+    ],
 
-      text: String,
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
+    person: {
+      type: String,
     },
-  ],
 
-  size: {
-    type: Number,
-    required: true,
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+
+    comments: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+
+        text: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+
+    size: {
+      type: Number,
+      required: true,
+    },
+
+    imgUrl: { type: String, rquired: true },
+
+    uploadedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-
-  imgUrl: { type: String, rquired: true },
-
-  uploadedAt: {
-    type: Date,
-    default: Date.now,
+  {
+    timeStamps: true,
   },
-}, 
-{
-    timeStamps: true
-}
-
 );
 
 const ImageModel = mongoose.model("Imagess", ImageSchema);
