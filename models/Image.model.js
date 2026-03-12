e6f87d22aeaca048a91ae3fae41d7f1d58e32b7a
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-03-12 09:23:56 +0530

    refactor: modified image schema

--- models/Image.model.js
+++ models/Image.model.js
@@ -1,48 +1,72 @@
-const { mongoose } = require("mongoose"); 
+const { mongoose } = require("mongoose");
 
 const ImageSchema = new mongoose.Schema({
-    imageId: {
-        type: String, 
-        required: true, 
-        unique: true
+  imageId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  albumId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Album",
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  tags: [
+    {
+      type: String,
+      trim: tue,
     },
+  ],
 
-    albumId: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Album', 
-        required: true
+  person: {
+    type: String,
+  },
+
+  isFavorite: {
+    type: Boolean,
+    default: false,
+  },
+
+  comments: [
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+
+      text: String,
+      createdAt: {
+        type: Data,
+        default: Date.now,
+      },
     },
+  ],
 
-    name:{
+  size: {
+    type: Number,
+    required: true,
+  },
 
-    }, 
+  imgUrl: { type: String, rquired: true },
 
-    tags: {
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, 
+{
+    timeStamps: true
+}
 
-    }, 
+);
 
-    person: {
-
-    }, 
-
-    isFavorite: {
-
-    }, 
-
-    comments: {
-
-    }, 
-
-    size: {
-
-    }, 
-
-    uploadedAt: {
-
-    },
-
-    imgUrl: {type: String, rquired: true},
-}); 
-
-const ImageModel = mongoose.model("Imagess", ImageSchema); 
-module.exports = { ImageModel }; 
+const ImageModel = mongoose.model("Imagess", ImageSchema);
+module.exports = { ImageModel };
