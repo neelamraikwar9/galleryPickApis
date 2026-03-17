678992e6aedd38de56b65e3449d47c3dfec24d93
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-03-17 08:01:49 +0530

    chore: modified images schema

--- models/Image.model.js
+++ models/Image.model.js
@@ -70,5 +70,5 @@ const ImageSchema = new mongoose.Schema(
   },
 );
 
-const ImageModel = mongoose.model("Image", ImageSchema);
+const ImageModel = mongoose.model("Imgs", ImageSchema);
 module.exports = { ImageModel };
