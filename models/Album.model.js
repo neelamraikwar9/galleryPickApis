1efbbca468b062e85a9653a98349ab4993f80052
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-05-18 12:51:45 +0530

    chore: modified album model

--- models/Album.model.js
+++ models/Album.model.js
@@ -34,6 +34,7 @@ const albumSchema = new mongoose.Schema(
           type: String,
           // required: true,
           lowercase: true,
+          accessLevel: { type: String, enum: ["view", "edit", "admin"] },
         },
 
         accessLevel: {
